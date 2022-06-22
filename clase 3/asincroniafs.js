92b0aff1bd31613be20c16e4a7594e9abee2d54e
ttronge <tomastronge@gmail.com>
2022-06-21 21:15:39 -0300

    exercise done

--- clase 3/asincroniafs.js
+++ clase 3/asincroniafs.js
@@ -12,7 +12,7 @@ class Conteneor {
     try {
       if (obj.id === 1) fs.writeFileSync(this.file, JSON.stringify([obj], null, 2));
       else {
-        const file = JSON.parse(fs.readFileSync(this.file));
+        const file =   JSON.parse(fs.readFileSync(this.file));
         file.push(obj);
         fs.writeFileSync(this.file, JSON.stringify(file, null, 2));
       };
@@ -26,12 +26,12 @@ class Conteneor {
   getById(id) {
     const file = JSON.parse(fs.readFileSync(this.file));
     const resultado = file.find((product) => product.id == id)
-    // console.log(resultado ? resultado : null);
+    console.log(resultado ? resultado : null);
   };
 
   getAll() {
     const file = JSON.parse(fs.readFileSync(this.file));
-    //   console.log(file);
+      console.log(file);
   };
 
   deleteById(id) {
@@ -52,9 +52,9 @@ const test1 = new Conteneor('./productos.txt');
 const test2 = new Conteneor('./productos.txt');
 const test3 = new Conteneor('./productos.txt');
 
-test1.save({ title: 'producto1', price: 5, description: 'tet' });
-test2.save({ title: 'test2', price: 5, description: 'tet' })
-test3.save({ title: 'test3', price: 5, description: 'tet' })
+test1.save({ title: 'producto1', price: 5, thumbnail: 'https://media.todojujuy.com/p/2eb76b994fbe3f471c06b21c836280e1/adjuntos/227/imagenes/000/352/0000352898/1200x1200/smart/el-sapo-pepe.jpg' });
+test2.save({ title: 'test2', price: 5, thumbnail: 'https://los40es00.epimg.net/los40/imagenes/2020/02/04/cinetv/1580834241_572538_1580834379_noticia_normal.jpg' })
+test3.save({ title: 'test3', price: 5, thumbnail: 'https://los40es00.epimg.net/los40/imagenes/2018/05/05/cinetv/1525519567_029601_1525519650_noticia_normal.jpg' })
 test1.getById(32)
 test1.getAll()
 test1.deleteById(3)
