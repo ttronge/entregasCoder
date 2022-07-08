c786a345bfad7bddfe16be07c7070c43cbfd8733
ttronge <tomastronge@gmail.com>
2022-07-07 21:11:57 -0300

    done exercise

--- clase5/app.js
+++ clase5/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const routerProducts = require('./routes/routerProductos');
 const app = express();
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
-app.use('/api/productos',routerProducts)
+app.use(express.static("public"));
 
-app.listen(8080);
+app.use('/api/productos', routerProducts);
+app.use('/uploads',express.static('uploads'))
+
+app.listen(8080, () => console.log("Its running in port 8080"));
+app.on("error", (error) => console.log("Hubo un error " + error));
--- clase5/routes/routerProductos.js
+++ clase5/routes/routerProductos.js
@@ -1,19 +1,66 @@
 const express = require('express');
+const path = require('path');
 const { Router } = express;
 const routerProducts = Router();
-let productos = []
-routerProducts.get('/', (req, res) =>{
-    res.send(productos)
+
+let productos = [];
+let numberId = 1;
+
+routerProducts.get('/seeForm', (req, res) => {
+    const pathFile = path.join(__dirname, '../public/index.html')
+    res.sendFile(pathFile)
+});
+
+
+routerProducts.get('/:id', (req, res) => {
+    const { params: { id } } = req;;
+    const buscador = productos.find(p => p.id == id);
+    if (buscador) res.json(buscador);
+    res.json({ error: "Producto no encontrado" });
+});
+routerProducts.get('/', (req, res) => {
+    res.json(productos)  
 })
 
 routerProducts.post('/', (req, res) => {
-    const {body} = req;
-    productos.push(body)
-    res.send(body)
+    const { body } = req;
+    body.id = numberId;
+    if (body.title && body.thumbnail) {
+        productos.push(body);
+        res.json(body);
+        numberId++
+    }
+    else {
+        res.json({ error: "Faltan caracterisictas al producto" });
+    };
 });
 
-routerProducts.put('/', (req, res) => res.send('products updated'));
+routerProducts.put('/:id', (req, res) => {
+    const { body: { title, thumbnail }, params: { id } } = req;
+    const buscador = productos.find(x => x.id == id);
+    if (buscador) {
+        productos.splice(id-1, 1);
+        buscador.title = title;
+        buscador.thumbnail = thumbnail;
+        productos.push(buscador);
+        res.json(buscador);
+    }
+    else {
+        res.json({ error: "Producto no encontrado" });
+    };
+});
 
-routerProducts.delete('/', (req, res) => res.send('products deleted'));
+routerProducts.delete('/:id', (req, res) => {
+    const { params: { id } } = req;
+    const buscador = productos.find(x => x.id == id);
+    if (buscador) {
+        let borrar = id - 1;
+        productos.splice(borrar, 1);
+        res.json(`se elimino el ${id}`);
+    }
+    else {
+        res.json({ error: "Producto no encontrado" });
+    };
+});
 
 module.exports = routerProducts;
