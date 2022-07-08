ba439dc63058868718eb5ab65915c9b48d3d71e9
ttronge <tomastronge@gmail.com>
2022-07-07 21:12:41 -0300

    done

--- clase5/routes/routerProductos.js
+++ clase5/routes/routerProductos.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { Router } = express;
 const routerProducts = Router();
 
-let productos = [];
+let productos = []; // se cargan los productos con el post :D
 let numberId = 1;
 
 routerProducts.get('/seeForm', (req, res) => {
